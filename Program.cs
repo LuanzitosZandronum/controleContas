de739bc5546051c2258879a835ed0dc0fe35961e
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-09-11 02:15:14 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using controleContas;
 
 /* atribuição de atributos antiga lol
@@ -16,15 +16,25 @@ conta1.Saldo = conta2.Saldo * 0.7m;
 conta1.Titular = cliente1;
 */
 
-Cliente cliente1 = new Cliente(123456, "José", 1990);
-Conta conta1 = new Conta(1, 1000m, cliente1);
+Cliente cliente1 = new Cliente("12345678910", "José", 1990);
+Banco banco1 = new Banco(10, "Budega");
+Agencia agencia1 = new Agencia(10, "170365-1", "32325657", banco1);
+Conta conta1 = new Conta(1, 1000m, cliente1, agencia1);
 
-Cliente cliente2 = new Cliente(654321, "João", 1980);
-Conta conta2 = new Conta(2, 500m, cliente2);
 
+
+Cliente cliente2 = new Cliente("10987654321", "João", 1980);
+Banco banco2 = new Banco(20, "Santana");
+Agencia agencia2 = new Agencia(20, "169242004", "265739", banco2);
+Conta conta2 = new Conta(2, 500m, cliente2, agencia2);
+
+
+
+
+/*
 string nomeTitular1 = conta1.Titular.Nome;
 string nomeTitular2 = conta2.Titular.Nome;
-
+*/
 //conta1.Saque(50m);  CHAMANDO FUNÇÃO DE SAQUE
 //conta1.Transferencia(conta2, 100m);   CHAMANDO FUNÇÃO DE TRANSFERÊNCIA
 
@@ -43,9 +53,14 @@ else
 
 //conta1.Numero = 123456;
 
-Console.WriteLine($"Número da Conta: {conta1.Numero} com saldo {conta1.Saldo}. Titular: {nomeTitular1}");
-Console.WriteLine($"Número da Conta: {conta2.Numero} com saldo {conta2.Saldo}. Titular: {nomeTitular2}");
+Console.WriteLine($"Número da Conta: {conta1.Numero} com saldo {conta1.Saldo}. Titular: {conta1.Titular.Nome}");
+Console.WriteLine($"Número da Conta: {conta2.Numero} com saldo {conta2.Saldo}. Titular: {conta2.Titular.Nome}");
 Console.WriteLine($"A conta com o maior saldo é a conta {maior}");
-Console.WriteLine($"{cliente1.Nome} = {cliente1.Cpf}");
+
+//Testes com Agência e Banco da Conta Número 1
+Console.WriteLine($"{cliente1.Nome} = CPF: {cliente1.Cpf}, Nascimento: {cliente1.anoNascimento}, {conta1.AgenciaReferente.Numero}");
+Console.WriteLine($"CEP da Agência: {conta1.AgenciaReferente.Cep}, Nome do Banco:{conta1.AgenciaReferente.BancoReferente.Nome}, Número do Banco: {conta1.AgenciaReferente.BancoReferente.Numero}");
+
+
 Console.WriteLine($"{cliente2.Nome} = {cliente2.Cpf}");
 Console.ReadLine();
