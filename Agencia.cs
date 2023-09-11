5baafda5cc80e54caef04ac896db91a4ab2b324a
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-09-11 02:15:45 -0300

    Add files via upload

---
+++ Agencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Agencia
+{
+	public Agencia(int numero, string cep, string telefoneContato, Banco banco)
+	{
+		Numero = numero;
+		Cep = cep;
+		TelefoneContato = telefoneContato;
+		BancoReferente = banco;
+	}
+	public int Numero { get; set; }
+	public string Cep { get; set; }
+	public string TelefoneContato { get; set; }
+	public Banco BancoReferente { get; set; }
+	
+}
---
+++ Banco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Banco
+{
+	public Banco(int numero, string nome)
+	{
+		Nome = nome;
+		Numero = numero;
+	}
+    public Banco() 
+	{ 
+		Nome = string.Empty;
+		Numero = 0;
+	}
+    public string Nome { get; set; }
+	public int Numero { get; set; }
+}
