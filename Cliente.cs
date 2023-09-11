e8828d0937dc857e8318329c95d7a78025b032cd
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-09-11 02:14:20 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +8,48 @@ namespace controleContas
 {
 	public class Cliente
 	{
-		public Cliente(long cpf, string nome, int anonascimento)
+		public Cliente(string cpf, string nome, int anonascimento)
 		{
-			if (2023 - anonascimento < 18)
+			if (string.IsNullOrWhiteSpace(nome))
 			{
-				throw new ArgumentException("Não deve ser menor de idade");
+				throw new ArgumentException("O nome do cliente não deve estar em branco.");
+			}
+			if (cpf == null)
+			{
+				throw new ArgumentException("O CPF do cliente deve ser informado.");
+			}
+			if (!ValidCPF(cpf)) 
+			{
+				throw new ArgumentException("O CPF do cliente deve conter exatamente 11 dígitos.");
+			}
+			if (DateTime.Now.Year - anonascimento < 18)
+			{
+				throw new ArgumentException("Não deve ser menor de idade.");
 			}
 			Cpf = cpf;
 			Nome = nome;
 			anoNascimento = anonascimento;
 		}
-		public Cliente() { }
-		public long Cpf { get; set; }
+		public Cliente() 
+		{ 
+			Cpf = string.Empty;
+			Nome = string.Empty;
+			anoNascimento = 0;
+		}
+
+		public string Cpf { get; set; }
 		public string Nome { get; set; }
 		public int anoNascimento { get; set; }
+
+		private static bool ValidCPF(string cpf)
+		{
+			string cpfStr = cpf.ToString().PadLeft(11, '0');
+
+			if (cpfStr.Length != 11 )
+			{
+				return false;
+			}
+			return true;
+		}
 	}
 }
