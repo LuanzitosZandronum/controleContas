3ef12f38c59bdae7d7e248685bde026eadb0aaea
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-09-12 00:11:04 -0300

    Update Banco.cs

--- Banco.cs
+++ Banco.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 
 public class Banco
 {
-	public Banco(int numero, string nome)
+	public Banco(int numero, string nome) //Método construtor da classe Banco que recebe dois parâmetros (numero e nome)
 	{
-		Nome = nome;
+        	// Define a propriedade Nome e Numero com o valor passado como argumento
+        	Nome = nome;
 		Numero = numero;
 	}
-    public Banco() 
+    public Banco() //Construtor padrão, sem parâmetros
 	{ 
 		Nome = string.Empty;
 		Numero = 0;
