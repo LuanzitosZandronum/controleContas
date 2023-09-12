88d922be8bc92bf0a764c5cb368f6ccbe91ed904
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-09-12 00:13:34 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -8,8 +8,8 @@ namespace controleContas
 {
 	public class Cliente
 	{
-		public Cliente(string cpf, string nome, int anonascimento)
-		{
+		public Cliente(string cpf, string nome, int anonascimento) //Método construtor da classe Cliente que recebe três parâmetros (cpf, nome e data de nascimento)
+        {
 			if (string.IsNullOrWhiteSpace(nome))
 			{
 				throw new ArgumentException("O nome do cliente não deve estar em branco.");
@@ -20,18 +20,19 @@ namespace controleContas
 			}
 			if (!ValidCPF(cpf)) 
 			{
-				throw new ArgumentException("O CPF do cliente deve conter exatamente 11 dígitos.");
+				throw new ArgumentException("O CPF do cliente deve ser válido.");
 			}
 			if (DateTime.Now.Year - anonascimento < 18)
 			{
 				throw new ArgumentException("Não deve ser menor de idade.");
 			}
+			// Define a propriedade Cpf, Nome e anoNascimento com o valor passado como argumento
 			Cpf = cpf;
 			Nome = nome;
 			anoNascimento = anonascimento;
 		}
-		public Cliente() 
-		{ 
+		public Cliente() //Construtor padrão, sem parâmetros
+        	{ 
 			Cpf = string.Empty;
 			Nome = string.Empty;
 			anoNascimento = 0;
@@ -41,14 +42,19 @@ namespace controleContas
 		public string Nome { get; set; }
 		public int anoNascimento { get; set; }
 
-		private static bool ValidCPF(string cpf)
+		private static bool ValidCPF(string cpf) //Método de Validação de CPF
 		{
-			string cpfStr = cpf.ToString().PadLeft(11, '0');
+            string cpfNumerico = new string(cpf.Where(char.IsDigit).ToArray());
 
-			if (cpfStr.Length != 11 )
+            if (cpfNumerico.Length != 11) //Se o CPF não tiver 11 Dígitos, ele será recusado
+            	{
+				return false;
+			}
+			if (cpfNumerico.Distinct().Count() == 1) //Se o CPF tiver apenas dígitos repetidos (ex: 11111111111), ele será recusado
 			{
 				return false;
 			}
+                
 			return true;
 		}
 	}
