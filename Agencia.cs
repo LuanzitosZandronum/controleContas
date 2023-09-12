ef52576a2e82914602c62e20f18fd5fd58eac1f3
LuanzitosZandronum <142948580+LuanzitosZandronum@users.noreply.github.com>
2023-09-12 00:11:34 -0300

    Update Agencia.cs

--- Agencia.cs
+++ Agencia.cs
@@ -4,6 +4,7 @@ public class Agencia
 {
 	public Agencia(int numero, string cep, string telefoneContato, Banco banco) ////Método construtor da classe Agencia que recebe quatro parâmetros (numero, cep, telefone e banco). A classe Banco encontrasse ligada diretamente aqui
     {
+		// Define a propriedade Numero, Cep, TelefoneContato e BancoReferente com o valor passado como argumento
 		Numero = numero;
 		Cep = cep;
 		TelefoneContato = telefoneContato;
